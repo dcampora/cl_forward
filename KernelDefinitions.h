e8ae88f6ebd76a0b41730e1809d221531e1a6e01
dcampora <dcampora@lab13.lbdaq.cern.ch>
2015-07-06 14:43:55 +0200

    Added KernelDefinitions.

---
+++ KernelDefinitions.h
@@ -0,0 +1,53 @@
+
+#define NUMTHREADS_X 64
+#define MAX_NUMTHREADS_Y 16
+#define NUM_ATOMICS 5
+#define USE_SHARED_FOR_HITS 1
+#define SH_HIT_MULT 2
+
+#define MAX_TRACKS 3000
+#define MAX_TRACK_SIZE 24
+
+#define REQUIRED_UNIQUES 0.6f
+#define MIN_HITS_TRACK 3
+#define MAX_FLOAT FLT_MAX
+#define MIN_FLOAT -FLT_MAX
+#define MAX_SKIPPED_MODULES 3
+#define TTF_MODULO 2000
+
+#define PARAM_W 3966.94f // 0.050 / sqrt( 12. )
+#define PARAM_MAXXSLOPE 0.4f
+#define PARAM_MAXYSLOPE 0.3f
+#define PARAM_MAXXSLOPE_CANDIDATES 0.4f
+
+#define PARAM_TOLERANCE 0.6f
+#define PARAM_TOLERANCE_CANDIDATES 0.6f
+
+#define MAX_SCATTER 0.000016f
+#define SENSOR_DATA_HITNUMS 3
+
+#define PRINT_SOLUTION false
+#define ASSERTS_ENABLED false
+
+#if ASSERTS_ENABLED == true
+#include "assert.h"
+#define ASSERT(EXPR) assert(EXPR);
+#else
+#define ASSERT(EXPR) 
+#endif
+
+struct Sensor {
+    unsigned int hitStart;
+    unsigned int hitNums;
+};
+
+struct Hit {
+    float x;
+    float y;
+    float z;
+};
+
+struct Track { // 4 + 24 * 4 = 100 B
+    unsigned int hitsNum;
+    unsigned int hits[MAX_TRACK_SIZE];
+};
